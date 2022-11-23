0d02baf4af139917fe124d79046a43393626866a
emoboyfriend <78619437+emoboyfriend@users.noreply.github.com>
2022-11-24 02:32:30 +0630

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+{
+  "videos": [
+    {
+      "name": "Football",
+      "versionCode": "1",
+      "description": "Update ပေးရင် User ကိုအသိပေးရေးရမယ့်စာ",
+      "directDownload": "",
+      "playstoreDownload": "",
+      "adChange": "admob",
+      "serverlink": "https://mygoaltv.xyz/",
+      "match": "{\n\"servername\":\"https://mygoaltv.xyz/\",\n\n\"elements\":\"div.match-list div.match\",\n\n\"league\":\"div.league\",\n\n\"date\":\"div.score span.vs\",\n\n\"time\":\"div.score div.time font\",\n\n\"home_team\":\"div.home-team img.team-logo\",\n\n\"home_img\":\"div.home-team img.team-logo\",\n\n\"away_team\":\"div.away-team img.team-logo\",\n\n\"away_img\":\"div.away-team img.team-logo\",\n\n\"live_href\":\"ul li.video-p a\"\n\n\n\n\n}",
+      "news": "{\n\"servername\":\"https://mysportmyanmar.com/\",\n\n\"newsArticle\":\"div.tabcontent div.main-blog-div.all-article div.withGrid.one-three\",\n\n\"newsCatagory\":\"div.category\",\n\n\"newsDate\":\"div.date\",\n\n\"newsTitle\":\"div.title\",\n\n\"newsThumbnail\":\"div.imageWrapper img\",\n\n\"newsLink\":\"a\"\n\n}",
+      "hilight": "{\n\"servername\":\"https://www.fasthighlights.net/tag/home/page/\",\n\n\"elements\":\"div.row div#aft-inner-row article\",\n\n\"title\":\"div.align-items-center div.spotlight-post figcaption h3 a\",\n\n\"tag\":\"div.align-items-center div.spotlight-post figure.categorised-article.inside-img div.categorised-article-wrapper div.figure-categories.figure-categories-bg ul.cat-links li a\",\n\n\"date\":\"div.align-items-center div.spotlight-post figcaption div.grid-item-metadata a\",\n\n\"thumbnail\":\"div.align-items-center div.spotlight-post figure.categorised-article.inside-img div.categorised-article-wrapper a img\",\n\n\"link\":\"div.align-items-center div.spotlight-post figure.categorised-article.inside-img div.categorised-article-wrapper a\"\n\n\n}"
+    }
+  ]
+}
